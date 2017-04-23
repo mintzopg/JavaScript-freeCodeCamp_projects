bf6a90d71109d4aeb7d1907f57285453cf3472dc
Giorgos <gimin@outlook.com>
2017-04-24 02:03:27 +0300

    adding everything be true

---
+++ EverythingBeTrue/script.js
@@ -0,0 +1,41 @@
+/**
+ * Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+ * 
+ * @param {any} collection 
+ * @param {any} pre 
+ * @returns 
+ */
+function truthCheck(collection, pre) {
+    // Is everyone being true?
+    let result
+    for (let obj of collection) {
+        result = false
+        for (let k of Object.keys(obj)) {
+            if (k === pre && Boolean(obj[k])) result = true
+        }
+        if (!result) break
+    }
+    return result
+}
+
+
+console.log(
+    truthCheck([{ "user": "Tinky-Winky", "sex": "male" }, { "user": "Dipsy", "sex": "male" }, { "user": "Laa-Laa", "sex": "female" }, { "user": "Po", "sex": "female" }], "sex"),
+    // should return true.
+    truthCheck([{ "user": "Tinky-Winky", "sex": "male" }, { "user": "Dipsy" }, { "user": "Laa-Laa", "sex": "female" }, { "user": "Po", "sex": "female" }], "sex"),
+    // should return false.
+    truthCheck([{ "user": "Tinky-Winky", "sex": "male", "age": 0 }, { "user": "Dipsy", "sex": "male", "age": 3 }, { "user": "Laa-Laa", "sex": "female", "age": 5 }, { "user": "Po", "sex": "female", "age": 4 }], "age"),
+    // should return false.
+    truthCheck([{ "name": "Pete", "onBoat": true }, { "name": "Repeat", "onBoat": true }, { "name": "FastFoward", "onBoat": null }], "onBoat"),
+    // should return false
+    truthCheck([{ "name": "Pete", "onBoat": true }, { "name": "Repeat", "onBoat": true, "alias": "Repete" }, { "name": "FastFoward", "onBoat": true }], "onBoat"),
+    // should return true
+    truthCheck([{ "single": "yes" }], "single"),
+    // should return true
+    truthCheck([{ "single": "" }, { "single": "double" }], "single"),
+    // should return false
+    truthCheck([{ "single": "double" }, { "single": undefined }], "single"),
+    // should return false
+    truthCheck([{ "single": "double" }, { "single": NaN }], "single")
+    // should return false
+)
