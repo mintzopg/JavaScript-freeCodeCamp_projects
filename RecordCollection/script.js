dfa47a1ac1bd2d2f239c5fd46350b1873c44fdc9
Giorgos <gimin@outlook.com>
2017-06-02 03:00:33 +0300

    Adding in advanced section

---
+++ RecordCollection/script.js
@@ -0,0 +1,60 @@
+// Setup
+var collection = {
+    "2548": {
+        "album": "Slippery When Wet",
+        "artist": "Bon Jovi",
+        "tracks": [
+            "Let It Rock",
+            "You Give Love a Bad Name"
+        ]
+    },
+    "2468": {
+        "album": "1999",
+        "artist": "Prince",
+        "tracks": [
+            "1999",
+            "Little Red Corvette"
+        ]
+    },
+    "1245": {
+        "artist": "Robert Palmer",
+        "tracks": []
+    },
+    "5439": {
+        "album": "ABBA Gold"
+    }
+};
+// Keep a copy of the collection for tests
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+
+// Only change code below this line
+function updateRecords(id, prop, value) {
+    if (prop !== 'tracks' && value !== '') {
+        collection[id][prop] = value;
+    }
+    if (prop === 'tracks') {
+        if (!collection[id].hasOwnProperty(prop)) {
+            collection[id][prop] = [];
+        }
+        if (value !== '') {
+            collection[id][prop].push(value);
+        }
+    }
+    if (value === '') {
+        delete collection[id][prop];
+    }
+
+    return collection;
+}
+
+// Alter values below to test your code
+updateRecords(5439, "artist", "ABBA");
+
+/*
+ After updateRecords(5439, "artist", "ABBA"), artist should be "ABBA"
+ After updateRecords(5439, "tracks", "Take a Chance on Me"), tracks should have "Take a Chance on Me" as the last element.
+ After updateRecords(2548, "artist", ""), artist should not be set
+ After updateRecords(1245, "tracks", "Addicted to Love"), tracks should have "Addicted to Love" as the last element.
+ After updateRecords(2468, "tracks", "Free"), tracks should have "1999" as the first element.
+ After updateRecords(2548, "tracks", ""), tracks should not be set
+ */
