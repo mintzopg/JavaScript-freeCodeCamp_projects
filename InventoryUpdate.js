0b794deeb26dbcf4192a45c16f78e9e05ce28235
Giorgos <mintzopg@users.noreply.github.com>
2017-11-27 11:26:19 +0200

    final
    freecodecamp advanced algorithm scripting

--- InventoryUpdate.js
+++ InventoryUpdate.js
@@ -1,5 +1,7 @@
-/* Compare and update the inventory stored in a 2D array against a second 2D array of a fresh delivery. Update the current existing inventory item quantities (in arr1). If an item cannot be found, add the new item and quantity into the inventory array. The returned inventory array should be in alphabetical order by item. */
-
+/* Compare and update the inventory stored in a 2D array against a second 2D array of a fresh delivery. 
+Update the current existing inventory item quantities (in arr1). 
+If an item cannot be found, add the new item and quantity into the inventory array. 
+The returned inventory array should be in alphabetical order by item. */
 
 function updateInventory(arr1, arr2) {
     // All inventory must be accounted for or you're fired!
@@ -28,7 +30,6 @@ function updateInventory(arr1, arr2) {
     arr.forEach(i => arrOut.push([i[1], i[0]]));
 
     return arrOut;
-
 }
 
 // TESTS
